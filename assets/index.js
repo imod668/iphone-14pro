d7bc85a2b068d21e98cc0d06506fd333fc867e48
imod668 <imod668@gmail.com>
2023-01-30 21:23:42 +0700

    add dynamic island

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
+//date & time
 function DateTime(){
 	const d = new Date();
 	const day = d.getDate();
@@ -17,6 +18,7 @@ function DateTime(){
 };
 setInterval(DateTime, 1000);
 
+//change background
 const wallArray = ["url('assets/imgs/iphone-14-pro-black.jpg')","url('assets/imgs/iphone-14-pro-gold.jpg')","url('assets/imgs/iphone-14-pro-purple.jpg')","url('assets/imgs/iphone-14-pro-silver.jpg')"];
 const changeWall = document.querySelector(".camera");
 let i = 0;
@@ -29,8 +31,38 @@ changeWall.addEventListener('click', () =>{
 	document.body.style.backgroundImage = wallArray[i];
 });
 
+//change font time
 let flashlight = document.querySelector(".flashlight");
 let fontclock = document.querySelector(".clock-area");
 flashlight.addEventListener('click', () =>{
 	fontclock.classList.toggle("clock-font");
+});
+
+//dynamic-island
+let dynamic_island = document.getElementById("dynamic_island");
+let wrapper = document.querySelector(".wrapper");
+let typing = document.querySelector(".dynamic-txt");
+
+dynamic_island.addEventListener('click', () =>{
+	const contain_class = dynamic_island.classList.contains('dynamic-island')
+	if(contain_class == true){
+		dynamic_island.classList.add("dynamic-island-fill");
+		dynamic_island.classList.remove("dynamic-island");
+		wrapper.style.visibility = "visible";
+		typing.classList.add('after');
+	}else{
+		dynamic_island.classList.remove("dynamic-island-fill");
+		dynamic_island.classList.add("dynamic-island");
+		wrapper.style.visibility = "hidden";
+		typing.classList.remove("after");
+
+	}
+});
+window.addEventListener('click', (e) => {
+	if (e.target !== dynamic_island){
+		dynamic_island.classList.remove("dynamic-island-fill");
+		dynamic_island.classList.add("dynamic-island");
+		wrapper.style.visibility = "hidden";
+		typing.classList.remove("after");
+	}
 });
